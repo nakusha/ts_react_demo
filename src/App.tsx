681a91321e22622a07d50d7d6954e12e013b3c29
정연수 <jys1015@hanyang.co.kr>
2020-02-14 17:17:23 +0900

    input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import Number from './Number';
+import { From, Input } from './input';
 
 // 이름앞에 대문자I를 붙여서 interface라는것을 알려줌
 interface IState {
   counter:number;
+  name:string;
+
 }
 
 class App extends Component<{}, IState> {
   state = {
-    counter:0
+    counter:0,
+    name:""
   };
   
   render (){
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
+        <From onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange}/>
+        </From>
         <Number count={counter}/><button onClick={this.add}>Add</button>
       </div>
     );
   };
+
+  onChange = (event:React.SyntheticEvent<HTMLInputElement>):void => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event:React.FormEvent) => {
+    event.preventDefault();
+  }
 
   add = ():void => {
     this.setState(prev => {
